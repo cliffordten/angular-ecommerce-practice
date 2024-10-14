21b8917485a7ec86e2ec99ac131f19edd6529ec3
cliffordten <cliffordteng5@gmail.com>
2024-10-14 07:51:48 +0100

    updated header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,80 +1,115 @@
 import { Component, OnInit } from '@angular/core';
 import { IHeaderTitle } from './interface';
 import { RouterLink } from '@angular/router';
-import { TextComponent } from "../text/text.component";
+import { TextComponent } from '../text/text.component';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 import { NgIconComponent } from '@ng-icons/core';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent,
+  HlmDialogDescriptionDirective,
+  HlmDialogFooterComponent,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import {
+  BrnDialogContentDirective,
+  BrnDialogTriggerDirective,
+} from '@spartan-ng/ui-dialog-brain';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
-
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, TextComponent, HlmIconComponent, HlmInputDirective, NgIconComponent, HlmButtonDirective],
+  imports: [
+    RouterLink,
+    TextComponent,
+    HlmIconComponent,
+    HlmInputDirective,
+    NgIconComponent,
+    HlmButtonDirective,
+    BrnDialogTriggerDirective,
+    BrnDialogContentDirective,
+    HlmDialogComponent,
+    HlmDialogContentComponent,
+    HlmDialogHeaderComponent,
+    HlmDialogFooterComponent,
+    HlmDialogTitleDirective,
+    HlmDialogDescriptionDirective,
+    HlmLabelDirective,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   isAuthenticated: boolean = false;
-  headerTitles: IHeaderTitle[][] = [[   {
-    title: "Home",
-    link: "/",
-    isLogo: true,
-    isIcon: false,
-    isSearch: false
-  },],
-[   {
-    title: "Search",
-    link: "/search",
-    isLogo: false,
-    isIcon: false,
-    isSearch: true
-  },],]
+  headerTitles: IHeaderTitle[][] = [
+    [
+      {
+        title: 'Home',
+        link: '/',
+        isLogo: true,
+        isIcon: false,
+        isSearch: false,
+      },
+    ],
+    [
+      {
+        title: 'Search',
+        link: '/search',
+        isLogo: false,
+        isIcon: false,
+        isSearch: true,
+      },
+    ],
+  ];
 
-  notAuthHeaderTitles: IHeaderTitle[] = [  {
-      title: "Login",
-      link: "/login",
+  notAuthHeaderTitles: IHeaderTitle[] = [
+    {
+      title: 'Login',
+      link: '/login',
       isLogo: false,
       isIcon: false,
-      isSearch: false
+      isSearch: false,
     },
     {
-      title: "SignUp",
-      link: "/signup",
+      title: 'SignUp',
+      link: '/signup',
       isLogo: false,
       isIcon: false,
-      isSearch: false
-    },]
+      isSearch: false,
+    },
+  ];
   authHeaderTitles: IHeaderTitle[] = [
     {
-      title: "Cart",
-      link: "/login",
+      title: 'Cart',
+      link: '/login',
       isLogo: false,
       isIcon: true,
-      isSearch: false
+      isSearch: false,
     },
     {
-      title: "Favorite",
-      link: "/signup",
+      title: 'Favorite',
+      link: '/signup',
       isLogo: false,
       isIcon: true,
-      isSearch: false
+      isSearch: false,
     },
     {
-      title: "User",
-      link: "/signup",
+      title: 'User',
+      link: '/signup',
       isLogo: false,
       isIcon: false,
-      isSearch: false
+      isSearch: false,
     },
-  ]
+  ];
   ngOnInit(): void {
     if (this.isAuthenticated) {
-      this.headerTitles.push(this.authHeaderTitles)
+      this.headerTitles.push(this.authHeaderTitles);
     } else {
-      this.headerTitles.push(this.notAuthHeaderTitles)
+      this.headerTitles.push(this.notAuthHeaderTitles);
     }
   }
 }
